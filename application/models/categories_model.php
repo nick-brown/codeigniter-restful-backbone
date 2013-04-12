08b6087df7feb10c047530b547a4c84f7ac1c806
Nick Brown <nbrown@ulsinc.com>
2013-04-12 14:02:43 -0700

    created categories model

---
+++ application/models/categories_model.php
@@ -0,0 +1,12 @@
+<?php
+
+    class Image_model extends Api_model {
+
+        protected $_table_name = 'ml_categories';
+        protected $_primary_key = 'id';
+
+        public function __construct()
+        {
+            parent::__construct();
+        }
+    }
